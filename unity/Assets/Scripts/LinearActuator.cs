cd16e246b5387a82822f6ad6fea11bd6c8400e3d
Luca Reccia <lucareccia@hotmail.it>
2020-06-12 17:31:55 +0200

    added script to control the actuator

--- unity/Assets/Scripts/LinearActuator.cs
+++ unity/Assets/Scripts/LinearActuator.cs
@@ -9,6 +9,8 @@ public class LinearActuator : MonoBehaviour
 
     private float min, max, stepSize;
     private Vector3 minForceForUpdateBug;
+    private ConfigurableJoint joint;
+    private Rigidbody movingRigidbody;
 
     // Start is called before the first frame update
     void Start(){
@@ -16,21 +18,45 @@ public class LinearActuator : MonoBehaviour
         max=1f;
         stepSize=(max-min)/stepsNumber;
 
+        joint=movingPiston.GetComponent<ConfigurableJoint>();
+        movingRigidbody=movingPiston.GetComponent<Rigidbody>();
+
         minForceForUpdateBug = new Vector3(0f,-0.1f,0f);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
         float direction = Input.GetAxis("Vertical");
         if(direction > 0){
             pistonExtend(1);
         } else if(direction < 0){
             pistonRetract(1);
         }
+    }
 
-        
-        /*float extensionDistance = transform.localScale.y * 10;
+    void pistonExtend(int multiplicator){
+        Vector3 anchor = joint.connectedAnchor;
+        anchor.y = Mathf.Min(anchor.y+multiplicator*stepSize, max);
+        updateMovingAnchor(anchor);
+    }
+
+    void pistonRetract(int multiplicator){
+        Vector3 anchor = joint.connectedAnchor;
+        anchor.y = Mathf.Max(anchor.y-multiplicator*stepSize, min);
+        updateMovingAnchor(anchor);
+    }
+
+    void updateMovingAnchor(Vector3 anchor){
+        joint.connectedAnchor = anchor;
+        movingRigidbody.AddForce(minForceForUpdateBug);
+    }
+}
+
+
+
+
+
+/*float extensionDistance = transform.localScale.y * 10;
         Vector3 minPosition=staticPiston.transform.position;
         Vector3 maxPosition=staticPiston.transform.position + staticPiston.transform.up*extensionDistance;
 
@@ -47,21 +73,3 @@ public class LinearActuator : MonoBehaviour
             else if(Vector3.Distance(maxPosition, movingPiston.transform.position) > extensionDistance)
                 movingPiston.transform.position = minPosition;
         }*/
-    }
-
-    void pistonExtend(int multiplicator){
-        Vector3 anchor = movingPiston.GetComponent<ConfigurableJoint>().connectedAnchor;
-        anchor.y = Mathf.Min(anchor.y+multiplicator*stepSize, max);
-        updateMovingAnchor(anchor);
-    }
-    void pistonRetract(int multiplicator){
-        Vector3 anchor = movingPiston.GetComponent<ConfigurableJoint>().connectedAnchor;
-        anchor.y = Mathf.Max(anchor.y-multiplicator*stepSize, min);
-        updateMovingAnchor(anchor);
-    }
-
-    void updateMovingAnchor(Vector3 anchor){
-        movingPiston.GetComponent<ConfigurableJoint>().connectedAnchor = anchor;
-        movingPiston.GetComponent<Rigidbody>().AddForce(minForceForUpdateBug);
-    }
-}
