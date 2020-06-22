f0431c31a4c93e53f67c1f6e2d0e9ec0fe0f70b7
Luca Reccia <lucareccia@hotmail.it>
2020-06-22 16:19:04 +0200

    added servo

--- unity/Assets/Scripts/Main.cs
+++ unity/Assets/Scripts/Main.cs
@@ -12,15 +12,25 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start(){
         rearPistonScript = rearPiston.GetComponent<LinearActuator>();
+        leftPistonScript = leftPiston.GetComponent<LinearActuator>();
+        rightPistonScript = rightPiston.GetComponent<LinearActuator>();
     }
 
     // Update is called once per frame
     void Update(){
-        float direction = Input.GetAxis("Vertical");
-        if(direction > 0){
+        if (Input.GetKey(KeyCode.Q))
+            leftPistonScript.pistonExtend(1);
+        else if (Input.GetKey(KeyCode.A))
+            leftPistonScript.pistonRetract(1);
+
+        if (Input.GetKey(KeyCode.E))
+            rightPistonScript.pistonExtend(1);
+        else if (Input.GetKey(KeyCode.D))
+            rightPistonScript.pistonRetract(1);
+
+        if (Input.GetKey(KeyCode.W))
             rearPistonScript.pistonExtend(1);
-        } else if(direction < 0){
+        else if (Input.GetKey(KeyCode.S))
             rearPistonScript.pistonRetract(1);
-        }
     }
 }
